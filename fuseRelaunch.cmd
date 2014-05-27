867df1b5311937d07720ae84de4ec6095caabee5
Jonas Rembser <jonas@linux-mswc.site>
2014-05-27 22:51:39 +0200

    removed almost everything

--- fuseRelaunch.cmd
+++
@@ -1,1 +0,0 @@
--intstyle "ise" -incremental -lib "unisims_ver" -lib "unimacro_ver" -lib "xilinxcorelib_ver" -o "C:/Users/Pol/ISE/PolJonasSnake/score_count_tb_isim_beh.exe" -prj "C:/Users/Pol/ISE/PolJonasSnake/score_count_tb_beh.prj" "work.score_count_tb" "work.glbl" 
--- gitREADME.txt
+++
@@ -1,7 +0,0 @@
-git status
-
-git add *
-git commit -m "MESSAGE"
-git push origin master
-
-git pull origin master
